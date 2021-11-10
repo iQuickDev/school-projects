a082fecf74035d06f148dc79cdec32eeff45d468
iQuickGaming <38632219+iQuickGaming@users.noreply.github.com>
2021-11-10 16:04:07 +0100

    Add files via upload

---
+++ Frontend/ToDoList/js/main.js
@@ -0,0 +1,22 @@
+var activities = document.querySelector("#activities").children[1]
+var index = activities.length
+
+document.querySelector("#add").onclick = () =>
+{
+    console.log(activities.children)
+
+    let activity = prompt("Cosa devi fare ?")
+    let newRow = document.createElement("tr")
+    let newCell = document.createElement("td")
+
+    newCell.textContent = activity
+    newRow.appendChild(newCell)
+    activities.appendChild(newRow)
+}
+
+document.querySelector("#delete").onclick = () =>
+{   
+    let deletionPos = activities.children.length == 0 ? alert("Non ci sono attività da eliminare") : prompt("Eliminare la riga... da 1 a " + activities.children.length)
+
+    activities.children[deletionPos - 1].remove()
+}
